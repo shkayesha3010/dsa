33d216025225f8d7922912b08000eb58efc5b757
Shaikh Ayesha <shaikhayesha3010@gmail.com>
2023-06-21 22:49:34 +0530

    even odd

--- Day 1/firstprogram.cpp
+++ Day 1/firstprogram.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
     int i = 51;
+    cout << " Even / Odd Program" << endl;        
     if(i%2==0){
         cout << " Even" << endl;        
     }else{
         cout << " Odd" << endl;        
     }
-  cout << " Salaam World" << endl;
   
   return 0; 
 }
