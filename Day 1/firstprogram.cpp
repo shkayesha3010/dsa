9161110094e606d9565b63a7b6b584c3370afd6d
Sadik Shaikh <abdullahsadik00@gmail.com>
2023-06-21 22:38:46 +0530

    first program

---
+++ Day 1/firstprogram.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i = 51;
+    if(i%2==0){
+        cout << " Even" << endl;        
+    }else{
+        cout << " Odd" << endl;        
+    }
+  cout << " Salaam World" << endl;
+  
+  return 0; 
+}
