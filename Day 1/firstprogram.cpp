49c1f15c6964f8ac7e25f7b6d4c86901e182ae5f
Shaikh Ayesha <shaikhayesha3010@gmail.com>
2023-06-22 23:21:41 +0530

    Data types

--- Day 1/firstprogram.cpp
+++ Day 1/firstprogram.cpp
@@ -1,14 +1,55 @@
-#include<iostream>
+
+
+#include <iostream>
 using namespace std;
 
-int main() {
-    int i = 51;
-    cout << " Even / Odd Program" << endl;        
-    if(i%2==0){
-        cout << " Even" << endl;        
-    }else{
-        cout << " Odd" << endl;        
-    }
-  
-  return 0; 
+int main()
+{
+// Datatype
+    cout << "Hello World" << endl;
+    int k = 4143;
+    cout << k << endl;
+    char b = 'n';
+    cout << b << endl;
+    bool bl = true;
+    cout << bl << endl;
+    bool nl = false;
+    cout << nl << endl;
+    float f = 1.23;
+    cout << f << endl;
+    double d = 1.97;
+    cout << d << endl;
+    int size = sizeof(d);
+    cout << size << endl;
+
+// Char
+    int a = 'a';
+    cout << a << endl;
+
+    char ch = 98;
+    cout << ch << endl;
+
+    char ch1 = 123456;
+    cout << ch1 << endl;
+
+    unsigned int a = 122;
+    cout << a << endl;
+
+    double f = 2.0 / 5;
+    cout << f << endl;
+    cout << 2.0 / 5 << endl;
+
+    int a = 2;
+    int b = 3;
+
+    bool first = (a == b);
+    cout << first << endl;
+
+    bool second = (a <= b);
+    cout << second << endl;
+
+    bool third = (a >= b);
+    cout << third << endl;
+    bool fourth = (a < b);
+    cout << fourth << endl;
 }
